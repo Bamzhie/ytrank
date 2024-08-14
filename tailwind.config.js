fad02ab38f91e162bfd3da474f9797f184701209
Bamzhie <sheuahmad@gmail.com>
2024-08-14 23:27:57 +0100

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "24rem",
+        md: "48rem",
+        lg: "56rem",
+        xl: "64rem",
+        ll: "80rem",
+        mm: "90rem",
+        nn: "102rem",
+        op: "160rem",
+      },
+    },
   },
   plugins: [],
 };
